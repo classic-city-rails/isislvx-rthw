c3511911df7bea6f4d43230e0dc45bbb810bc729
isislvx <isislvx@yahoo.com>
2014-09-25 19:56:20 -0400

    Example 2

---
+++ ex2.rb
@@ -0,0 +1,5 @@
+#I like playing with Ruby Code
+puts "I could have code like this." #and this is how it goes
+#You can also use the comment to "disable" or comment out a piece of the code:
+#puts "This won't run."
+puts "This will run."
