897ea761bb2fe46fd04729f2b6fc1dd045ff66ab
isislvx <isislvx@yahoo.com>
2014-09-29 12:48:51 -0400

    Example 9

---
+++ ex9.rb
@@ -0,0 +1,15 @@
+# Here are some more strange things, remember to type it exactly
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "here are the days: #{days}"
+puts "here are the months: #{months}"
+
+puts """
+
+There's something going on here. 
+With three double quotes. 
+We'll be able to type as much as we like to.
+Even 4 lines if we want or 5, or 6.
+"""
